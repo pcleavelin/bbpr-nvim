248b784a492755b3422c9e55439350885e75f14f
Patrick Cleavelin <patrick@spacegirl.nl>
2022-07-30 17:18:37 -0500

    got ui kinda working, next is diffing from git

--- lua/bbpr/init.lua
+++ lua/bbpr/init.lua
@@ -1,7 +1,37 @@
 -- A simple interface for viewing pull requests in a BitBucket repository
 
 local api = vim.api
-local buf, win, pr_list = nil
+local pr_list, pr = nil
+
+local choose_buf, desc_buf, file_list_buf = nil
+local pr_choose_win, pr_desc_win, pr_diff_win = nil
+
+-- Taken from https://gist.github.com/GabrielBdeC/b055af60707115cbc954b0751d87ec23
+function string:split(delimiter)
+    local result = {}
+    local from = 1
+    local delim_from, delim_to = string.find(self, delimiter, from, true)
+    while delim_from do
+        if (delim_from ~= 1) then
+            table.insert(result, string.sub(self, from, delim_from-1))
+        end
+        from = delim_to + 1
+        delim_from, delim_to = string.find(self, delimiter, from, true)
+    end
+    if (from <= #self) then table.insert(result, string.sub(self, from)) end
+    return result
+end
+
+local function set_mappings(buf, mappings)
+    for k,v in pairs(mappings) do
+        api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"bbpr".'..v..'<cr>',
+            {
+                nowait = true,
+                noremap = true,
+                silent = true
+            })
+    end
+end
 
 local function get_pr_list()
     pr_list = {
@@ -13,15 +43,34 @@ local function get_pr_list()
     }
 end
 
-local function open_window()
-    -- create a new scratch buffer
-    buf = api.nvim_create_buf(false, true)
+local function get_pr(link)
+    pr = {
+        ["desc"] = "This is a test description for a test Pull Request from Bitbucket.\nThough this is not actually from bitbucket.",
+        ["branch"] = "master",
+    }
+end
 
+local function set_buf_options(buf, mappings)
     -- set buffer to delete when hidden
     api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
     api.nvim_buf_set_option(buf, 'buftype', 'nofile')
     api.nvim_buf_set_option(buf, 'swapfile', false)
     api.nvim_buf_set_option(buf, 'filetype', 'pat-bbpr')
+    api.nvim_buf_set_option(buf, 'modifiable', false)
+
+    if mappings ~= nil then
+        set_mappings(buf, mappings)
+    end
+end
+
+local function open_pr_choose_window()
+    -- create a new scratch buffer
+    choose_buf = api.nvim_create_buf(false, true)
+    set_buf_options(choose_buf, {
+        q = 'close()',
+        ['<cr>'] = 'open_pr()'
+    })
+
 
     local width = api.nvim_get_option("columns")
     local height = api.nvim_get_option("lines")
@@ -42,68 +91,120 @@ local function open_window()
         col = col
     }
 
-    win = api.nvim_open_win(buf, true, opts)
+    pr_choose_win = api.nvim_open_win(choose_buf, true, opts)
+    api.nvim_win_set_option(pr_choose_win, 'cursorline', true)
 end
 
-local function close()
+local function create_split_win(vertical)
+    if vertical then
+        vim.cmd("vs")
+    else
+        vim.cmd("split")
+    end
+
+    -- make sure to move to the new window
+    vim.cmd("wincmd l")
+
+    local win = api.nvim_get_current_win()
+    local buf = api.nvim_create_buf(false, true)
+
+    set_buf_options(buf)
+    api.nvim_win_set_buf(win, buf)
+
+    return win, buf
+end
+
+local function close(win)
     if win and api.nvim_win_is_valid(win) then
         api.nvim_win_close(win, true)
     end
 end
 
-local function open_pr()
-    -- split by spaces and grab the first element
-    local path = api.nvim_get_current_line():gmatch("%S+")()
+local function paint_file_list_buf()
+    api.nvim_buf_set_option(file_list_buf, 'modifiable', true)
 
-    local pr = pr_list[path]
-    
-    print(pr)
+    local result = vim.cmd('silent exec ":r !git diff HEAD~1 --name-only"');
+
+    api.nvim_buf_set_option(file_list_buf, 'modifiable', false)
 end
 
-local function paint()
-    api.nvim_buf_set_option(buf, 'modifiable', true)
+local function paint_desc()
+    api.nvim_buf_set_option(desc_buf, 'modifiable', true)
+
+    local list = pr["desc"]:split("\n")
+
+    api.nvim_buf_set_lines(desc_buf, 0, -1, false, list)
+
+    api.nvim_buf_set_option(desc_buf, 'modifiable', false)
+end
+
+local function paint_choose_buf()
+    api.nvim_buf_set_option(choose_buf, 'modifiable', true)
 
     local list = {}
     for k,v in pairs(pr_list) do
         table.insert(list, #list + 1, k..' <=> '..v["link"])
     end
 
-    api.nvim_buf_set_lines(buf, 0, -1, false, list)
+    api.nvim_buf_set_lines(choose_buf, 0, -1, false, list)
 
-    api.nvim_buf_set_option(buf, 'modifiable', false)
+    api.nvim_buf_set_option(choose_buf, 'modifiable', false)
 end
 
-local function set_mappings()
-    local mappings = {
-        q = 'close()',
-        ['<cr>'] = 'open_pr()'
-    }
+local function open_pr()
+    -- split by spaces and grab the first element
+    local pr_index = api.nvim_get_current_line():gmatch("%S+")()
+    local pr = pr_list[pr_index]
+    get_pr(pr["link"])
 
-    for k,v in pairs(mappings) do
-        api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"bbpr".'..v..'<cr>',
-            {
-                nowait = true,
-                noremap = true,
-                silent = true
-            })
-    end
+    close(pr_choose_win)
+    choose_buf = nil
+
+    api.nvim_command("tabnew")
+
+    -- setup window showing the file diff
+    pr_diff_win = api.nvim_get_current_win()
+    diff_buf = api.nvim_get_current_buf()
+    set_buf_options(diff_buf)
+
+    -- setup file chooser
+    pr_file_list_win, file_list_buf = create_split_win(true)
+    api.nvim_win_set_option(pr_file_list_win, 'cursorline', true)
+    set_mappings(file_list_buf, {
+        ['<cr>'] = 'load_diff()'
+    })
+    paint_file_list_buf()
+
+    -- setup window showing PR description
+    pr_desc_win, desc_buf = create_split_win(false)
+    set_buf_options(desc_buf)
+    paint_desc()
+
+    api.nvim_set_current_win(pr_file_list_win)
+end
+
+local function load_diff()
+    local file = api.nvim_get_current_line()
+    api.nvim_set_current_win(pr_diff_win)
+
+
 end
 
 local function bbpr()
-    if win and api.nvim_win_is_valid(win) then
-        api.nvim_set_current_win(win)
+    if win and api.nvim_win_is_valid(pr_choose_win) then
+        api.nvim_set_current_win(pr_choose_win)
     else
-        open_window()
+        open_pr_choose_window()
     end
 
     get_pr_list()
-    set_mappings()
-    paint()
+    paint_choose_buf()
 end
 
 return {
     bbpr = bbpr,
-    open_window = open_window,
+    open_pr_choose_window = open_pr_choose_window,
     close = close,
     open_pr = open_pr,
+    load_diff = load_diff
 }
